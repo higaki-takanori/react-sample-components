8ae16a98dcfb4f477391874a3cc5c7e2276b1bf0
higaki-codesandbox <garoumare@gmail.com>
2023-01-16 12:05:06 +0000

    create organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "aaa",
+  image: "https://source.unsplash.com/T-0EW-SEbsE",
+  email: "aaa@example.com",
+  phone: "1234-1234-123",
+  company: {
+    name: "test company",
+  },
+  website: "https://google.com",
+};
 
 export default function App() {
   return (
@@ -9,6 +21,7 @@ export default function App() {
       <PrimaryButton>button</PrimaryButton>
       <SecondaryButton>second</SecondaryButton>
       <SearchInput></SearchInput>
+      <UserCard user={user} />
     </div>
   );
 }
